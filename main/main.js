88c7c3b7dbda296de79e771272b270df15208628
xxxxxthhh <xxxxxthhh@gmail.com>
2019-05-24 22:01:02 +0800

    try to refactor again...

--- main/main.js
+++ main/main.js
@@ -1,12 +1,18 @@
 module.exports = function main(inputs) {
-    dis = inputs.distance;
-    mins = inputs.parkTime;
+    var dis = inputs.distance;
+    var mins = inputs.parkTime;
+
+    var mile_price = 0.8
+    var start_price = 6
+    var wait_fee = 0.25
+
+    var basic_price = wait_fee * mins + start_price
 
     if (dis < 2) {
-        return Math.round(0.25 * mins + 6);
+        return Math.round(basic_price);
     } else if (dis >= 8) {
-        return Math.round(0.25 * mins + 6 + 0.8 * 6 + 1.2 * (dis - 8));
+        return Math.round(basic_price + mile_price * (8-2) + 1.5 * mile_price * (dis - 8));
     } else {
-        return Math.round(0.25 * mins + 6 + 0.8 * (dis - 2));
+        return Math.round(basic_price + mile_price * (dis - 2));
     }
 };
