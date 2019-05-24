12d830e49b15f9d4fdaf98ddd7b9ec1e2748074a
xxxxxthhh <xxxxxthhh@gmail.com>
2019-05-24 19:41:17 +0800

    pass first & second specs

--- main/main.js
+++ main/main.js
@@ -1,7 +1,7 @@
 module.exports = function main(inputs) {
     // write your code here...
     if (inputs.distance < 6){
-        return Math.round(6+0.8*inputs.parkTime)
+        return Math.round(6+0.25*inputs.parkTime)
     }
     return "price";
 };
