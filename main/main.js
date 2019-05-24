d653ba2547636f6a1c6a834fd27de3fabfb0d947
xxxxxthhh <xxxxxthhh@gmail.com>
2019-05-24 21:01:59 +0800

    REFACTOR.

--- main/main.js
+++ main/main.js
@@ -1,13 +1,12 @@
 module.exports = function main(inputs) {
-    // write your code here...
     dis = inputs.distance;
     mins = inputs.parkTime;
 
     if (dis < 2) {
-        return Math.round(6 + 0.25 * mins);
+        return Math.round(0.25 * mins + 6);
     } else if (dis >= 8) {
-        return Math.round(10.8 + 1.2 * (dis - 8) + 0.25 * mins);
+        return Math.round(0.25 * mins + 6 + 0.8 * 6 + 1.2 * (dis - 8));
     } else {
-        return Math.round(6 + 0.8 * (dis - 2) + 0.25 * mins);
+        return Math.round(0.25 * mins + 6 + 0.8 * (dis - 2));
     }
 };
