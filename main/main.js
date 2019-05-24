d236760bc52bd6a26c4bd00051c716b268fbeee3
xxxxxthhh <xxxxxthhh@gmail.com>
2019-05-24 20:30:51 +0800

    pass all specs

--- main/main.js
+++ main/main.js
@@ -1,12 +1,13 @@
 module.exports = function main(inputs) {
     // write your code here...
-    dis = inputs.distance
-    mins = inputs.parkTime
+    dis = inputs.distance;
+    mins = inputs.parkTime;
 
-    if (dis < 2){
-        return Math.round(6+0.25*mins)
-    } else if (2 <= dis < 8){
-        return Math.round(6+0.8*(dis-2)+0.25*mins)
+    if (dis < 2) {
+        return Math.round(6 + 0.25 * mins);
+    } else if (dis >= 8) {
+        return Math.round(10.8 + 1.2 * (dis - 8) + 0.25 * mins);
+    } else {
+        return Math.round(6 + 0.8 * (dis - 2) + 0.25 * mins);
     }
-    return "price";
 };
