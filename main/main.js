b678dae7cee551b3623e43de50c0199ac2b34189
xxxxxthhh <xxxxxthhh@gmail.com>
2019-05-24 19:49:08 +0800

    pass third & forth specs

--- main/main.js
+++ main/main.js
@@ -1,7 +1,12 @@
 module.exports = function main(inputs) {
     // write your code here...
-    if (inputs.distance < 6){
-        return Math.round(6+0.25*inputs.parkTime)
+    dis = inputs.distance
+    mins = inputs.parkTime
+
+    if (dis < 2){
+        return Math.round(6+0.25*mins)
+    } else if (2 <= dis < 8){
+        return Math.round(6+0.8*(dis-2)+0.25*mins)
     }
     return "price";
 };
